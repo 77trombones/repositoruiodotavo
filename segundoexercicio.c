dfb181e19397284524550d0d5bbb18530abc316f
gustavo de andrade venâncio <gustavovenanandrade@gmail.com>
2025-02-19 20:44:38 -0300

    Add files via upload

---
+++ segundoexercicio.c
@@ -0,0 +1,87 @@
+int main() {
+  char letra = 'Y';
+  char proxima = letra + 1;
+  printf("Letra: %c", proxima);
+  return 0;
+}  
+
+#include <stdio.h>
+
+int main() {
+  char letra = 'Y';
+  char proxima = letra + 2;
+  printf("Letra: %c", proxima);
+  return 0;
+}  
+
+#include <stdio.h>
+
+int main() {
+  char letra = 'Y';
+  char proxima = letra + 3;
+  printf("Letra: %c", proxima);
+  return 0;
+}  
+
+#include <stdio.h>
+
+int main() {
+  char letra = 'Y';
+  char proxima = letra + 4;
+  printf("Letra: %c", proxima);
+  return 0;
+}  
+
+#include <stdio.h>
+
+int main() {
+  char letra = 'Y';
+  char proxima = letra + 5;
+  printf("Letra: %c", proxima);
+  return 0;
+}  
+
+#include <stdio.h>
+
+int main() {
+  char letra = 'Y';
+  char proxima = letra + 6;
+  printf("Letra: %c", proxima);
+  return 0;
+}  
+
+#include <stdio.h>
+
+int main() {
+  char letra = 'Y';
+  char proxima = letra + 7;
+  printf("Letra: %c", proxima);
+  return 0;
+}  
+
+#include <stdio.h>
+
+int main() {
+  char letra = 'Y';
+  char proxima = letra + 8;
+  printf("Letra: %c", proxima);
+  return 0;
+}  
+
+#include <stdio.h>
+
+int main() {
+  char letra = 'Y';
+  char proxima = letra + 9;
+  printf("Letra: %c", proxima);
+  return 0;
+}  
+
+#include <stdio.h>
+
+int main() {
+  char letra = 'Y';
+  char proxima = letra + 10;
+  printf("Letra: %c", proxima);
+  return 0;
+}
